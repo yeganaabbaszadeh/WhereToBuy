418a94e2996582e06c6b8f1af66f9546b2c49392
yeganaabbaszadeh <yegana.abbaszadeh.std@bhos.edu.az>
2021-04-02 08:19:59 +0400

    changed

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+	return render_template('about.html', title='About')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
