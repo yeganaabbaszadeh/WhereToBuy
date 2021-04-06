429f63509debe6eee968672fd5ecab9cec1f0e1b
yeganaabbaszadeh <yegana.abbaszadeh.std@bhos.edu.az>
2021-04-06 20:17:13 +0400

    Login and sign up form added, some design changes

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from website import create_app
 
-app = Flask(__name__, template_folder="website/templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-db = SQLAlchemy(app)
+app = create_app()
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/about")
-def about():
-	return render_template("about.html", title='About')
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
+
+# from flask import Flask, render_template, request, redirect
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
+
+# app = Flask(__name__, template_folder="website/templates")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+# db = SQLAlchemy(app)
+
+
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
+
+# @app.route("/about")
+# def about():
+# 	return render_template("about.html", title='About')
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ website/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "ZD!c$8L4#1%q$as6vWo7"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
+
+
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
+
+# @app.route("/about")
+# def about():
+# 	return render_template("about.html", title='About')
+
