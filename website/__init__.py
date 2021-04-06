bf0c25d90a52a40910c26a397085c5c28fb541e8
yeganaabbaszadeh <yegana.abbaszadeh.std@bhos.edu.az>
2021-04-06 20:58:54 +0400

    some changes

--- website/__init__.py
+++ website/__init__.py
@@ -21,12 +21,3 @@ def create_app():
 
     return app
 
-
-# @app.route("/")
-# def home():
-#     return render_template("home.html")
-
-# @app.route("/about")
-# def about():
-# 	return render_template("about.html", title='About')
-
