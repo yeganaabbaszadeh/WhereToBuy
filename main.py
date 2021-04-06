89fa51cf083b9f8f4b1e546168f6651cfab9b1ca
yeganaabbaszadeh <yegana.abbaszadeh.std@bhos.edu.az>
2021-04-06 13:36:59 +0400

    main.py added

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="website/templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(app)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-	return render_template('about.html', title='About')
+	return render_template("about.html", title='About')
 
 
 if __name__ == "__main__":
