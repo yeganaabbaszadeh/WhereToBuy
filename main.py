d2d0368cc01826f8f2abbb013620a98961b7767d
yeganaabbaszadeh <yegana.abbaszadeh.std@bhos.edu.az>
2021-04-07 03:15:25 +0400

    additional functions

--- main.py
+++ main.py
@@ -6,24 +6,3 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# from flask import Flask, render_template, request, redirect
-# from flask_sqlalchemy import SQLAlchemy
-# from datetime import datetime
-
-# app = Flask(__name__, template_folder="website/templates")
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-# db = SQLAlchemy(app)
-
-
-# @app.route("/")
-# def home():
-#     return render_template("home.html")
-
-# @app.route("/about")
-# def about():
-# 	return render_template("about.html", title='About')
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from os import path
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
@@ -12,12 +14,30 @@ def create_app():
     app.config['SECRET_KEY'] = "ZD!c$8L4#1%q$as6vWo7"
     app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
-    
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")
 
