5d2d58ebf2474120f7e48a64e2bd0b8858d30e9b
yeganaabbaszadeh <yegana.abbaszadeh.std@bhos.edu.az>
2021-04-27 15:57:05 +0400

    filtering options added

--- website/views.py
+++ website/views.py
@@ -8,6 +8,8 @@ amazon_data = []
 amazon_ls = []
 tapaz_data = []
 tapaz_ls = []
+aliexpress_data = []
+aliexpress_ls = []
 
 
 @views.route('/', methods=["GET", "POST"])
@@ -20,13 +22,19 @@ def about():
 	return render_template("about.html", title='About', user=current_user)
 
 
-@views.route('/search/', methods=['GET', 'POST'])
+@views.route('/search', methods=['GET', 'POST'])
 def search():
     item = request.form['search_item']
     amazon_data.clear()
     tapaz_data.clear()
     amazonScraper(item)
     tapazScraper(item)
+    aliexpressScraper(item)
+
+    if request.method == "POST":
+        sites = request.form.getlist('site')
+
+    print(sites)
 
     with open('webscrapers/results.csv', mode='r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -38,7 +46,7 @@ def search():
                         row['price'] =  '$' + row['price']
                     else:
                         row['price'] = "Pricing information not available." 
-                    
+                                
                     amazon_ls = [row['title'], 'https://www.amazon.com/' + row['link'], row['price'], row['page']]
                     amazon_data.append(amazon_ls)
 
@@ -47,10 +55,20 @@ def search():
                         row['price'] = row['price'] + 'AZN'
                     else:
                         row['price'] = "Pricing information not available." 
-                    
+                            
                     tapaz_ls = [row['title'], 'https://tap.az' + row['link'], row['price'], row['page']]
                     tapaz_data.append(tapaz_ls)
 
+                elif row['page'] == 'aliexpress':
+                    if row['price'] != "":
+                        row['price'] = row['price']
+                    else:
+                        row['price'] = "Pricing information not available." 
+
+                    if row['title'] != []:
+                        aliexpress_ls = [row['title'], 'https:' + row['link'], row['price'], row['page']]
+                        aliexpress_data.append(aliexpress_ls)
+
             line_num = line_num + 1
 
-    return render_template('home.html', user=current_user, isSearching = True, amazonItems=amazon_data, tapazItems=tapaz_data, item=request.form.get('search_item'))
+    return render_template('home.html', user=current_user, isSearching = True, amazonItems=amazon_data, tapazItems=tapaz_data, aliexpressItems=aliexpress_data, item=request.form.get('search_item'), sites=request.form.getlist('site'))
